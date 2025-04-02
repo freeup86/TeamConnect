8a5853be884b4762c3d6663249215ae3db5b9bed
cortez <lcortez86@icloud.com>
2025-04-02 12:43:31 -0700

    New projects

---
+++ service-registry/src/test/kotlin/com/teamconnect/serviceregistry/ServiceRegistryApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.teamconnect.serviceregistry
+
+import com.teamconnect.registry.ServiceRegistryApplication
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(classes = [ServiceRegistryApplication::class])
+class ServiceRegistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
